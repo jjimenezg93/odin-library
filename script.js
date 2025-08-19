638abb9854d7f077ef43cb62ce33154e27646f4c
Julián Jiménez <jjimenezg93@gmail.com>
2025-08-19 20:25:21 +0100

    Add basic displayBooks
    Still have to style cards properly

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ addBookToLibrary("The Fellowship of the Ring", "J.R.R. Tolkien", 398);
 addBookToLibrary("Pride and Prejudice", "Jane Austen", 279);
 
 console.log(library);
+
+function displayBooks() {
+  let mainElem = document.querySelector("main");
+  library.forEach((book) => {
+    let bookCard = document.createElement("article");
+    bookCard.classList.add("book-card");
+
+    let bookTitle = document.createElement("h1");
+    bookTitle.textContent = book.title;
+    let bookAuthor = document.createElement("p");
+    bookAuthor.textContent = book.author;
+    let bookPages = document.createElement("p");
+    bookPages.textContent = book.pages;
+
+    bookCard.append(bookTitle, bookAuthor, bookPages);
+
+    mainElem.appendChild(bookCard);
+  });
+}
+
+displayBooks();
